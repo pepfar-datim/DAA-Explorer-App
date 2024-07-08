c03b6b22b5a6509d281c5f5155aee10895d05d42
Christian Onyango <christianonyango92@gmail.com>
2024-07-08 19:06:12 +0300

    2022.0.2 (#24)
    * pushing changes for testing
    
    * added paws package to server file
    
    * added paws package to server file
    
    * new changes added
    
    * dynamic dates fixed, size of scatterplot increased
    
    * changes on cache
    
    * updated latest analytics package
    
    * changes from Rshiny for message where no data is available for PEPFAR/MOH, and manifest file update
    
    * version update
    
    * updated the tag version
    
    * updated some packages and fixed issue with picking the current fiscal year
    
    * fixed fiscal year dynamic pick
    
    * title change
    
    * added new authentication method

--- server.R
+++ server.R
@@ -7,6 +7,35 @@ library(rpivotTable)
 library(gt)
 library(DT)
 library(paws)
+
+
+if(interactive()){
+  options(shiny.port = 3734)
+  APP_URL <- "http://127.0.0.1:3734/"
+} else {
+  APP_URL <- Sys.getenv("APP_URL")
+}
+
+
+oauth_app <- httr::oauth_app(
+  Sys.getenv("OAUTH_APPNAME"),
+  key = Sys.getenv("OAUTH_KEYNAME"),
+  secret = Sys.getenv("OAUTH_SECRET"),
+  redirect_uri = APP_URL
+)
+
+oauth_api <- httr::oauth_endpoint(
+  base_url = paste0(Sys.getenv("DATIM_URL"), "uaa/oauth"),
+  authorize = "authorize",
+  access = "token"
+)
+
+oauth_scope <- "ALL"
+
+has_auth_code <- function(params) {
+  return(!is.null(params$code))
+}
+
 shinyServer(function(input, output, session) {
   # Reactive Values -----------------------------------------------------------
 
@@ -27,6 +56,57 @@ shinyServer(function(input, output, session) {
   data <- reactiveValues(gg_con = c(), # ggplot2 graph data for the Concordancy Graph
                          tb_pivot = c()) # rpivotTable data based on the current view in the pivot table
 
+  observeEvent(input$login_button_oauth, {
+    params <- parseQueryString(session$clientData$url_search)
+    if (has_auth_code(params)) {
+      token <- httr::oauth2.0_token(
+        app = oauth_app,
+        endpoint = oauth_api,
+        scope = oauth_scope,
+        use_basic_auth = TRUE,
+        oob_value = APP_URL,
+        cache = FALSE,
+        credentials = httr::oauth2.0_access_token(
+          endpoint = oauth_api,
+          app = oauth_app,
+          code = params$code,
+          use_basic_auth = TRUE
+        )
+      )
+      loginAttempt <- tryCatch({
+        datimutils::loginToDATIMOAuth(
+          base_url = Sys.getenv("DATIM_URL"),
+          token = token,
+          app = oauth_app,
+          api = oauth_api,
+          redirect_uri = APP_URL,
+          scope = oauth_scope,
+          d2_session_envir = parent.env(environment())
+        )
+      }, error = function(e) {
+        futile.logger::flog.info(paste0("User login failed. ", e$message), name = "daa-analysis")
+      })
+
+      if (exists("d2_default_session")) {
+        user_input$authenticated <- TRUE
+        user_input$d2_session <- d2_default_session$clone()
+        user_input$authorized <- credential_check(d2_session = user_input$d2_session)
+        if (user_input$authorized) {
+          datimutils::loginToDATIM(
+            base_url = Sys.getenv("GEOALIGN_URL"),
+            username = Sys.getenv("GEOALIGN_USERNAME"),
+            password = Sys.getenv("GEOALIGN_PASSWORD"),
+            d2_session_envir = parent.env(environment())
+          )
+          user_input$geo_session <- d2_default_session$clone()
+          futile.logger::flog.info(paste0("User ", user_input$d2_session$me$userCredentials$username, " logged in."), name = "daa-analysis")
+        } else {
+          futile.logger::flog.info(paste0("User ", user_input$d2_session$me$userCredentials$username, " not authorized."), name = "daa-analysis")
+        }
+      }
+    }
+  })
+
   # Reactives -----------------------------------------------------------------
 
   analysis_data <- reactive({
@@ -43,7 +123,6 @@ shinyServer(function(input, output, session) {
     }
 
     # Return the `d` list object with data
-
     return(d)
   })
 
@@ -79,68 +158,24 @@ shinyServer(function(input, output, session) {
   # Wipes out all session objects for active logins
   # Changes status of "authorized" and "authenticated" to FALSE
   # Changes status of "Ready" to FALSE
+
   observeEvent(input$logout, {
-    flog.info(paste0("User ",
-                     user_input$d2_session$me$userCredentials$username,
-                     " logged out."))
-    ready$ok  <-  FALSE
-    user_input$authenticated  <-  FALSE
-    user_input$authorized  <-  FALSE
-    user_input$d2_session  <-  NULL
-    user_input$geo_session  <-  NULL
+    req(input$logout)
+    updateQueryString("?", mode = "replace", session = session)
+    futile.logger::flog.info(paste0("User ", user_input$d2_session$me$userCredentials$username, " logged out."))
+    ready$ok <- FALSE
+    user_input$authenticated <- FALSE
+    user_input$authorized <- FALSE
+    user_input$d2_session <- NULL
+    user_input$geo_session <- NULL
     d2_default_session <- NULL
     gc()
     session$reload()
   })
 
-  observeEvent(input$login_button, {
-    base_url <- Sys.getenv("DATIM_URL")
-    print(paste0("Base URL: ", base_url))
-    tryCatch({
-      datimutils::loginToDATIM(username = input$user_name,
-                               password = input$password,
-                               base_url = base_url,
-                               d2_session_envir = parent.env(environment()))
-    },
-    #This function throws an error if the login is not successful
-    error = function(e) {
-      sendSweetAlert(
-        session,
-        title = "Login failed",
-        text = "Please check your username/password!",
-        type = "error")
-      futile.logger::flog.info(
-        paste0("User ", input$user_name, " login failed."),
-        name = "daa-analysis")
-    })
 
-    if (exists("d2_default_session")) {
-      user_input$authenticated <- TRUE
-      user_input$d2_session <- d2_default_session$clone()
+  #COMMENT HERE
 
-      # Checks users credentials to make sure they are authorized to use app
-      user_input$authorized <-
-        credential_check(d2_session = user_input$d2_session)
-
-      if (user_input$authorized == TRUE) {
-        # Logs into GeoAlign using stored credentials for the app
-        datimutils::loginToDATIM(base_url = Sys.getenv("GEOALIGN_URL"),
-                                 username = Sys.getenv("GEOALIGN_USERNAME"),
-                                 password = Sys.getenv("GEOALIGN_PASSWORD"),
-                                 d2_session_envir = parent.env(environment()))
-        user_input$geo_session <- d2_default_session$clone()
-        print(user_input$geo_session)
-
-        futile.logger::flog.info(
-          paste0("User ", input$user_name, " logged in."),
-          name = "daa-analysis")
-      } else {
-        futile.logger::flog.info(
-          paste0("User ", input$user_name, " not authorized."),
-          name = "daa-analysis")
-      }
-    }
-  })
 
   # Observes country dropdown menu and
   # updates `user_input$ou_uid` reactive value to match `input$ou` value
@@ -189,16 +224,118 @@ shinyServer(function(input, output, session) {
   # Progresses a user through various UI screens based on whether
   # they have successfully logged into their DATIM account and
   # whether they are authorized to access this app.
+
   output$ui <- renderUI({
-    if (user_input$authenticated == FALSE) { # If a user is not authenticated, show them the login screen
-      callModule(ui_login, "ui")
-    } else if (user_input$authorized == FALSE) { # If a user is not authorized, show them the "unauthorized" screen
-      callModule(ui_unauth, "ui")
-    } else { # If a user is both authenticated and authorized, show them the main app content
-      callModule(ui_main, "main",
-                 d2_session = user_input$d2_session)
+    if (user_input$authenticated == FALSE) {
+      fluidPage(
+        fluidRow(
+          column(width = 2, offset = 5,
+                 br(), br(), br(), br(),
+                 wellPanel(
+                   fluidRow(img(src = 'pepfar.png', align = "center")),
+                   fluidRow(h4("Welcome to the DATIM DAA Explorer tool. Please login with your DATIM credentials:")),
+                   fluidRow(actionButton("login_button_oauth", "Log in with DATIM")),
+                   uiOutput("ui_hasauth"),
+                   uiOutput("ui_redirect"),
+                   tags$hr()
+                 )
+          )
+        )
+      )
+    } else if (user_input$authorized == FALSE) {
+      fluidPage(h4("Unauthorized access"))
+    } else {
+      callModule(ui_main, "main", d2_session = user_input$d2_session)
     }
   })
+
+  output$ui_redirect <- renderUI({
+    if (!is.null(input$login_button_oauth)) {
+      if (input$login_button_oauth > 0) {
+        url <- httr::oauth2.0_authorize_url(oauth_api, oauth_app, scope = oauth_scope)
+        redirect <- sprintf("location.replace(\"%s\");", url)
+        tags$script(HTML(redirect))
+      } else NULL
+    } else NULL
+  })
+
+  observeEvent(input$login_button_oauth > 0, {
+    params <- parseQueryString(session$clientData$url_search)
+    req(has_auth_code(params))
+    token <- httr::oauth2.0_token(
+      app = oauth_app,
+      endpoint = oauth_api,
+      scope = oauth_scope,
+      use_basic_auth = TRUE,
+      oob_value = APP_URL,
+      cache = FALSE,
+      credentials = httr::oauth2.0_access_token(endpoint = oauth_api,
+                                                app = oauth_app,
+                                                code = params$code,
+                                                use_basic_auth = TRUE)
+    )
+
+    loginAttempt <- tryCatch({
+      datimutils::loginToDATIMOAuth(
+        base_url = Sys.getenv("DATIM_URL"),
+        token = token,
+        app = oauth_app,
+        api = oauth_api,
+        redirect_uri = APP_URL,
+        scope = oauth_scope,
+        d2_session_envir = parent.env(environment())
+      )
+    }, error = function(e) {
+      flog.info(paste0("User login failed. ", e$message), name = "daa-analysis")
+    })
+
+    if (exists("d2_default_session")) {
+      user_input$authenticated <- TRUE
+      user_input$d2_session <- d2_default_session$clone()
+      #d2_default_session <- NULL
+      user_input$authorized <- credential_check(d2_session = user_input$d2_session)
+      if (user_input$authorized) {
+        datimutils::loginToDATIM(
+          base_url = Sys.getenv("GEOALIGN_URL"),
+          username = Sys.getenv("GEOALIGN_USERNAME"),
+          password = Sys.getenv("GEOALIGN_PASSWORD"),
+          d2_session_envir = parent.env(environment())
+        )
+        user_input$geo_session <- d2_default_session$clone()
+        futile.logger::flog.info(paste0("User ", user_input$d2_session$me$userCredentials$username, " logged in."), name = "daa-analysis")
+      } else {
+        futile.logger::flog.info(paste0("User ", user_input$d2_session$me$userCredentials$username, " not authorized."), name = "daa-analysis")
+      }
+      # if(user_input$authorized == TRUE){
+      #   futile.logger::flog.info(
+      #     # paste0(
+      #     #   "User ",
+      #     #   user_input$d2_session$me$userCredentials$username,
+      #     #   " logged in."
+      #     # ),
+      #     paste0("User ", user_input$user_name, " logged in."),
+      #     name = "daa-analysis"
+      #   )
+      # } else {
+      #   futile.logger::flog.info(
+      #     paste0(
+      #       "User ",
+      #       user_input$d2_session$me$userCredentials$username,
+      #       " not authorized."
+      #     ),
+      #     name = "daa-analysis"
+      #   )
+      # }
+
+      # foo <- getUserOperatingUnits(user_input$d2_session$user_orgunit, d2session = user_input$d2_session)
+      # user_input$user_ous <- setNames(foo$id, foo$name)
+
+
+    }
+  })
+
+
+
 
   # Table and Graph Outputs ---------------------------------------------------
 
@@ -222,16 +359,18 @@ shinyServer(function(input, output, session) {
     gg_con
   })
 
+
   ## Reporting Graph --------------------------------------------------------
   output$reporting_graph <- renderPlot({
     gg_rprt <- analysis_data() %>%
       matching_summary(filter_values = filter_values) %>%
       site_reporting_graph()
 
-    data$gg_rprt <- gg_rprt
+    data$gg_rprt < gg_rprt
 
     gg_rprt
   })
+
 
   ## Scatterplot Graph --------------------------------------------------------
   output$interactive_scatter <- renderPlotly({
